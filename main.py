2b2d76d7db5056510797cfd1408f4429b7b7dff5
ninad <ninad2eyes@gmail.com>
2023-04-27 02:00:49 +0530

    added url endpoints for yearly and station-wise search

--- main.py
+++ main.py
@@ -15,14 +15,25 @@ def home():
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     # data_small / TG_STAID000001.txt
-    print(f"data_small/TG_STAID000{station.zfill(3)}.txt")
+    # print(f"data_small/TG_STAID000{station.zfill(3)}.txt")
     df = pd.read_csv(f"data_small//TG_STAID000{station.zfill(3)}.txt",
                      skiprows=20,
                      parse_dates=["    DATE"])
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"date": f"{date}", "station": station, "temperature": temperature}
 
-
+@app.route("/api/v1/station/<station>")
+def station(station):
+    df = pd.read_csv(f"data_small//TG_STAID000{station.zfill(3)}.txt",
+                     skiprows=20,
+                     parse_dates=["    DATE"])
+    return df.to_dict(orient="records")
+@app.route("/api/v1/year/<station_year>/<year>")
+def yearly(station_year, year):
+    df = pd.read_csv(f"data_small//TG_STAID000{station_year.zfill(3)}.txt",
+                     skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    return df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
 
 if __name__ == "__main__":
     app.run(debug=True, port=port)
