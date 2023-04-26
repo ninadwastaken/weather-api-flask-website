f68f61bb2b831984a0312c74ef92b5ff03eaf082
ninad <ninad2eyes@gmail.com>
2023-04-27 01:11:58 +0530

    added stations table to homepage

--- side_project.py
+++ side_project.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
+import pandas
 
 mean = Flask(__name__)
 
+
 @mean.route("/")
 def home():
-    return render_template("side_project.html")
+    return render_template("side_project.html",
+                           data=table.to_html())
 
 @mean.route("/meaningapi/v1/<word>")
 def meaning(word):
-    return {"definition": word.upper(), "word": word}
+    filename = "dictionary.csv"
+    df = pandas.read_csv(filename)
+
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
+    return {"definition": definition, "word": word}
 
 if __name__ == "__main__":
     mean.run(debug=True)
